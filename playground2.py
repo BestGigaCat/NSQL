31adcef4986b1abc3c9541c569e3c6237d727a04
Shiming Ren <shimingr@Shimings-iMac.local>
2023-11-06 01:52:48 -0800

    add file to excercise add new embedding

--- playground2.py
+++ playground2.py
@@ -1,12 +1,30 @@
+import chromadb
+
+# from chromadb.config import Settings
+# import OllamaEmbeddings
+from langchain.embeddings import OllamaEmbeddings
+
+# from langchain.vectorstores import Chroma
+print("Loading")
+persistent_client = chromadb.PersistentClient("./chroma_db")
+# collection = client.create_collection("lilianblog")
+collection = persistent_client.get_or_create_collection(name="playground2")
+print(persistent_client.list_collections())
+
+# Set up embedding
 embed = OllamaEmbeddings(base_url="http://localhost:11434", model="llama2")
-vectorstore = Chroma.from_documents(documents=all_splits, embedding=embed, persist_directory="./chroma_db")
+embeddings1 = embed.embed_query(text="How I get employee's title?")
 
-print("ask")
-# Retrieve
-question = "How can Task Decomposition be done?"
-docs = vectorstore.similarity_search(question)
-print("end")
+# Adding to collection
+collection.add(ids=["2"], embeddings=[embeddings1], metadatas=[{"query": "SELECT count(*) from Employee;"}])
+
+# Generating Embeddings for golden query
+print("Generate embedings")
+question = "Can you generate a SQL script to get the total number of employees?"
+embeddings2 = embed.embed_query(text=question)
+
+# Search query
+print("Search docs")
+docs = collection.query(query_embeddings=embeddings2)
 print(len(docs))
-
-# print the current date and time
-print("Current time is: ", datetime.now())
+print(docs)
